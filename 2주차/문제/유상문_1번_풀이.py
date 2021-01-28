b35d5370dc7df6442a57259458648cc6f1bbd3fb
smyoo <zepetto.yoo@gmail.com>
2021-01-28 17:58:13 +0900

    개념 수정
    딕셔너리 개념 추가 및 오타 수정

--- 2주차/문제/유상문_1번_풀이.py
+++ 2주차/문제/유상문_1번_풀이.py
@@ -3,6 +3,13 @@ import collections
 participant = list(map(str,input().split()))
 completion = list(map(str,input().split()))
 temp = []
+#collctions의 counter 함수
+def solution(participant, completion) :
+    result = collections.Counter(participant) - collections.Counter(completion)
+    return list(result)[0]
+
+print(solution(participant, completion))
+
 # 아래 풀이는 시간초과
 # def solution(participant, completion) :
 #     participant.sort()
@@ -18,9 +25,10 @@ temp = []
 #             participant.remove(_)
 #     return participant[0]
 
-#collctions의 counter 함수
-def solution(participant, completion) :
-    result = collections.Counter(participant) - collections.Counter(completion)
-    return list(result)[0]
-
-print(solution(participant, completion))
+#중복제거
+# def solution(p,c):
+#     set1 = set(p)
+#     set2 = set(c)
+#     set3 =  set1-set2
+# 
+#     return set3
