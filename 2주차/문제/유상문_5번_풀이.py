71fa3bf0e2e07931bf86f0b5e1e649a0d7f781a3
유상문 <zepetto.yoo@gmail.com>
2021-01-31 11:17:27 +0900

    문제 풀이 추가
    - 2주차 힙&해시 스터디 시간 문제 풀이 추가 (2문제)

--- 2주차/문제/유상문_5번_풀이.py
+++ 2주차/문제/유상문_5번_풀이.py
@@ -12,33 +12,37 @@ n = int(input())
 
 clothes = []
 
-for i in range(n):
+for i in range(n) :
     len = int(input())
     one_clothe = []
-    for j in range(len):
+    for j in range(len) :
         one_clothe.append(input().split())
     clothes.append(one_clothe)
 
+result_list = []
 
-def solution(clothes):
-    result = 1
 
-    for i in range(n):
+def solution(clothes) :
+    for i in range(n) :
+        result = 1
         clothedict = {}
         temp_clothes = clothes[i]
-        result_list =[]
-        for clothe in temp_clothes:
+        for clothe in temp_clothes :
             key = clothe[1]
 
-            if key in clothedict.keys():
-                clothedict[key]=clothedict[key]+1
-            else:
-                clothedict[key]=1
+            if key in clothedict.keys() :
+                clothedict[key] = clothedict[key] + 1
+            else :
+                clothedict[key] = 1
 
-        for item in clothedict:
-            result  *= clothedict[item]+1
+        for item in clothedict :
+            result *= clothedict[item] + 1
 
         result_list.append(result - 1)
         clothedict.clear()
+
+    for _ in result_list:
+        print(_)
+
 
 solution(clothes)
--- 2주차/문제/유상문_6번_풀이.py
+++ 2주차/문제/유상문_6번_풀이.py
@@ -1,4 +1,20 @@
 import heapq
+import sys
+
+n = int(sys.stdin.readline())
+list = []
+
+while n > 0 :
+    num = int(sys.stdin.readline())
+    if num > 0 :
+        heapq.heappush(list, num)
+    elif num == 0 :
+        if len(list) > 0 :
+            print(heapq.heappop(list))
+        else :
+            print(0)
+    n -= 1
+
 # 9
 # 0
 # 12345678
@@ -9,18 +25,3 @@ import heapq
 # 0
 # 0
 # 32
-import sys
-
-n = int(sys.stdin.readline())
-list = []
-while n > 0 :
-    num = int(sys.stdin.readline())
-    if num > 0 :
-        heapq.heappush(list, num)
-    elif num ==0 :
-        if len(list) > 0:
-            heapq.heapify(list)
-            print(heapq.heappop(list))
-        else :
-          print(0)
-    n -= 1
