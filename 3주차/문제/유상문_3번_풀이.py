ad549c01b3f416488d632809d9025734660c26b7
smyoo <zepetto.yoo@gmail.com>
2021-02-02 20:18:21 +0900

    3번 풀이 수정

--- 3주차/문제/유상문_3번_풀이.py
+++ 3주차/문제/유상문_3번_풀이.py
@@ -5,13 +5,16 @@ def solution(number, k):
     item = list("1924")
     temp = list(combinations(item, len(item) - 2))
     numbers = []
-    for str in temp:
+    for char in temp:
         num = ''
-        for i in str:
+        for i in char:
             num += i
         numbers.append(int(num))
 
     numbers.sort()
-    return ''.join(numbers.reverse())
+    result = numbers[len(numbers)-1]
+
+    return str(result)
 
 print(solution(1234,2))
+print('hello')
