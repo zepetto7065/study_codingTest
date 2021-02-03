0c7a8f341e6e8f3c27b599fdbff23096f54b38b5
smyoo <zepetto.yoo@gmail.com>
2021-02-03 22:19:41 +0900

    4번풀이 추가

--- 3주차/문제/유상문_4번_풀이.py
+++ 3주차/문제/유상문_4번_풀이.py
@@ -1,10 +1,24 @@
-people = [70,50,80,50]
+from collections import deque
+
+people = [70, 80, 50, 20, 30]
 limit = 100
 
 def solution(people, limit):
     answer = 0
+    people.sort()
+    people = deque(people)
+    while people:
+        if len(people) == 1:
+            answer += 1
+            break
+        else:
+            if people[0] + people[-1]> limit:
+                people.pop()
+                answer += 1
+            else:
+                people.popleft()
+                people.pop()
+                answer += 1
+    return answer
 
-
-
-
-return answer
+print(solution(people,limit))
