61f07e78c4442f452a1cc114d010dd11856b102d
smyoo <zepetto.yoo@gmail.com>
2021-02-04 23:15:20 +0900

    문법 정리 추가

--- 3주차/문제/유상문_3번_풀이.py
+++ 3주차/문제/유상문_3번_풀이.py
@@ -1,20 +1,28 @@
 from itertools import combinations
 
 
-def solution(number, k):
-    item = list("1924")
-    temp = list(combinations(item, len(item) - 2))
-    numbers = []
-    for char in temp:
-        num = ''
-        for i in char:
-            num += i
-        numbers.append(int(num))
+def solution(number, k) :
+    answer = ''
+    numbers = list(number)
+    l = len(numbers) - k
 
-    numbers.sort()
-    result = numbers[len(numbers)-1]
+    index = 0
+    slice = k
 
-    return str(result)
+    for i in range(l):
+        slice_numbers = numbers[index :index + slice]
 
-print(solution(1234,2))
+        if len(slice_numbers) >= slice:
+
+            max_num = max(slice_numbers)
+            index += numbers.index(max_num) + 1
+            slice -= 1
+            answer += max_num
+        else:
+            answer += numbers[len(numbers)-1]
+    return answer
+
+# print(solution("1924",2))
+print(solution("1231234",3))
+# print(solution("4177252841",4))
 print('hello')
